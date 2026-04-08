ab785d17cd7fc99f4def5fdd8edb8a7f93d8eb23
Vaibhavi-Roop <vaibhavi.golluru@outlook.com>
2026-04-08 18:36:37 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hi!")
+print ("What's your name?")
+print(5)
+print("Hi are you 12 years old?")
+print("This is my fav number", 6)
+print("My name is Vaibhavi",end = "." )
+v = 'hi'
+print (v, "What are you doing?")
+a = "Daisy"
+print("hello", a, "\n welcome to the world of coding")
