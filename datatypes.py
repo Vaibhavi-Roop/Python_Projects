547d0c9fcefc36872451d2ab187e02bd8ebb381e
Vaibhavi-Roop <vaibhavi.golluru@outlook.com>
2026-04-08 19:17:19 +1000

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,11 @@
+#Datatypes tell what kind of value will be stored in a particular variable.
+# e.g a = 6, a is the 'jar' but datatype is the 'label', what the 'jar' contains.
+a = 6
+print("datatype of a is", type(a))
+b = 4.6
+print("datatype of b is", type(b))
+c = "coding"
+print("datatype of c is",type(c))
+
+d = True 
+print("datatype of d is",type(d))
---
+++ typecasting.py
@@ -0,0 +1,9 @@
+name = "Vaibhavi"
+print("Name:", name)
+age = 12
+print ("Age:", age)
+weight = 60.4
+print("Weight:", weight)
+weight =  int(weight)
+print("New weight:", weight)
+print("datatype of new weight is", type(weight))
