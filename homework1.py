7dfd11137e6c979344824e6190890b3abe21fae7
Vaibhavi-Roop <vaibhavi.golluru@outlook.com>
2026-04-09 13:39:01 +1000

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,18 @@
+birthday = input('Which friends brithday do you want to know:')
+friend_1 = '25th February'
+friend_2 = '8th March'
+friend_3 = '12th May'
+friend_4 = '1st January'
+friend_5 = '2nd May'
+if birthday == 'Friend 1':
+    print(friend_1)
+elif birthday == 'Friend 2':
+    print(friend_2)
+elif birthday == 'Friend 3':
+    print(friend_3)
+elif birthday == 'Friend 4':
+    print(friend_4)
+elif birthday == 'Friend 5':
+    print(friend_5)
+else:
+    print('Error')
