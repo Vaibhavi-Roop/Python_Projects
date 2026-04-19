ea104315c3acd1783006588fa1c097e937c67365
Vaibhavi-Roop <vaibhavi.golluru@outlook.com>
2026-04-19 12:42:58 +1000

    Add files via upload

---
+++ and_or_operators.py
@@ -0,0 +1,6 @@
+number_1 = int(input("Enter number:"))
+number_2 = int(input("Enter another number:"))
+if number_1 > 10 and number_2 > 10:
+    print("Both numbers are greater than 10.")
+if number_1 > 10 or number_2 > 10:
+    print("At least one number is greater than 10.")
---
+++ not_operators.py
@@ -0,0 +1,16 @@
+a = int(input("Enter a number:"))
+b = int(input("Enter another number:"))
+c = int(input("Enter a third number:"))
+if not (a==b):
+    print ("A and B are not equal.")
+else:
+    print ("They are equal.")
+
+print(not(b==c))
+ 
+p = input("Enter a word:")
+q = input("Another word:")
+if not (p==q):
+    print("They are not equal:")
+else:
+    print("They are equal.")
