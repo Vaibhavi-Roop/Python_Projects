896f09593d3a61d6d6f1fadfc69443dbd7a4a8df
Vaibhavi-Roop <vaibhavi.golluru@outlook.com>
2026-04-20 15:21:28 +1000

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,5 @@
+character = input("Enter a single character:")
+if ('A' <= character <= 'Z') or ('a' <= character <= 'z'):
+    print("It is an alphabet.")
+else:
+    print("It is not an alphabet.")
