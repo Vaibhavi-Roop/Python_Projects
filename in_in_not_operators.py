3530495c984300e78640ffa2440902f72ef83ee9
Vaibhavi-Roop <vaibhavi.golluru@outlook.com>
2026-04-22 19:43:57 +1000

    Add files via upload

---
+++ in_in_not_operators.py
@@ -0,0 +1,15 @@
+numbers = [4, 37, 284, 39377, 92, 74, 103, 19375]
+n = int(input("Pick any number:"))
+if n in numbers:
+    print("We have your number!")
+else:
+    print("Sorry, we don't have your lucky number. Try again!")
+
+
+Havenfield = ("flaredon", "tyrannosaurus rex", "verminion", "alicorn", "imp")
+print('Gorgodon' in Havenfield)
+print('Iggy' in Havenfield)
+print('Verdi' in Havenfield)
+print('flaredon' in Havenfield)
+print('imp'in Havenfield)
+print('alicorn' in Havenfield)
---
+++ is_is_not_operators.py
@@ -0,0 +1,7 @@
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+b = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+c = a
+print(a is c)
+print(a is b)
+print(a is not b)
+print(a is not c)
