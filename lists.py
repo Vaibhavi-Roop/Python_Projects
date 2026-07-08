1eb2a352b8b829501c76e9a9bc63f4e1745cc236
Vaibhavi-Roop <vaibhavi.golluru@outlook.com>
2026-07-08 19:34:45 +1000

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,12 @@
+empty_list = []
+print(empty_list)
+
+number_list = [1,2,3,4,5]
+print(number_list)
+
+triple_list = [1,2,3]*3
+print(triple_list)
+
+reverse_list = [100,200,300,400,500]
+reverse_list = reverse_list[::-1]
+print(reverse_list)
