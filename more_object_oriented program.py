89314715e3c98222eef2c7dc90104ff2572bb831
Vaibhavi-Roop <vaibhavi.golluru@outlook.com>
2026-08-02 12:50:45 +1000

    Add files via upload

--- more_object_oriented program.py
+++ more_object_oriented program.py
@@ -8,3 +8,17 @@ class IOString:
 string_1 = IOString()
 string_1.get_string()
 string_1.show_string()
+
+class employee:
+    def __init__(self):
+        print("Employee created")
+
+    def __del__(self):
+        print("Destructor called")
+def create_obj(self):
+    print("Creating object")
+    obj = employee()
+    return obj
+print("Calling create object function")
+obj = create_obj
+print("Program end")
